08fac97304c299c47641adc80c66602b0673fa04
Nada-Rjb <enadaragab2020@gmail.com>
2025-02-13 03:31:13 +0200

    Refactor navigation toggle functionality to prevent event propagation and add close behavior on document click; introduce fade-out animation for improved user experience.

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
 const navToggle = document.querySelector('[area-control="primary-navigation"]');
 const primaryNav = document.querySelector("#primary-navigation");
 
-navToggle.addEventListener("click", () => {
-  console.log("clicked button");
+// navToggle.addEventListener("click", () => {
+//   console.log("clicked button");
+//   const navOpen = navToggle.getAttribute("area-expanded");
+//   if (navOpen === "false") {
+//     navToggle.setAttribute("area-expanded", "true");
+//   } else {
+//     navToggle.setAttribute("area-expanded", "false");
+//   }
+//   console.log(navOpen);
+// });
+//open
+navToggle.addEventListener("click", (event) => {
   const navOpen = navToggle.getAttribute("area-expanded");
+  event.stopPropagation(); //  Prevents document click event from firing
+
   if (navOpen === "false") {
     navToggle.setAttribute("area-expanded", "true");
   } else {
     navToggle.setAttribute("area-expanded", "false");
   }
-  console.log(navOpen);
+});
+//closewhen click in any thing in page
+document.addEventListener("click", () => {
+  const navOpen = navToggle.getAttribute("area-expanded");
+  if (navOpen === "true") {
+    navToggle.setAttribute("area-expanded", "false");
+  }
 });
 
 const resizeObserver = new ResizeObserver(() => {
