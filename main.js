9254d4ed47e658f9020bc0abf0e11b1279cf27e1
Nada-Rjb <enadaragab2020@gmail.com>
2025-03-01 14:59:27 +0200

    Refactor navigation toggle functionality to improve event handling and prevent document click propagation; add menu icon for better user experience.

--- main.js
+++ main.js
@@ -1,17 +1,6 @@
 const navToggle = document.querySelector('[area-control="primary-navigation"]');
 const primaryNav = document.querySelector("#primary-navigation");
-
-// navToggle.addEventListener("click", () => {
-//   console.log("clicked button");
-//   const navOpen = navToggle.getAttribute("area-expanded");
-//   if (navOpen === "false") {
-//     navToggle.setAttribute("area-expanded", "true");
-//   } else {
-//     navToggle.setAttribute("area-expanded", "false");
-//   }
-//   console.log(navOpen);
-// });
-//open
+const menueIcon = navToggle.querySelector("img");
 navToggle.addEventListener("click", (event) => {
   const navOpen = navToggle.getAttribute("area-expanded");
   event.stopPropagation(); //  Prevents document click event from firing
